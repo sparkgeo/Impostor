accc194da7c0a4e7598551cd521aaa82f317e447
Aarthi Babu <babu@unbc.ca>
2016-11-26 19:04:46 -0800

    bugfix

--- impostor/backend.py
+++ impostor/backend.py
@@ -4,19 +4,10 @@ import django.contrib.auth as auth
 from django.contrib.auth.models import Group
 from django.http import HttpRequest
 from models import ImpostorLog
+from rest_framework import authentication
+from rest_framework import exceptions
 
 from django.conf import settings
-
-try:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-except ImportError:
-    from django.contrib.auth.models import Use
-
-try:
-    IMPOSTOR_GROUP = Group.objects.get(name=settings.IMPOSTOR_GROUP)
-except:
-    IMPOSTOR_GROUP = None
 
 
 def find_request():
@@ -37,51 +28,83 @@ def find_request():
     return request
 
 
-class AuthBackend:
+class AuthBackend(authentication.BaseAuthentication):
     supports_anonymous_user = False
     supports_object_permissions = False
     supports_inactive_user = False
 
-    def authenticate(self, username=None, password=None):
+    def authenticate(username=None, password=None):
+
+        try:
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+        except ImportError:
+            from django.contrib.auth.models import User
+
+        try:
+            impostor_group = Group.objects.get(name=settings.IMPOSTOR_GROUP)
+        except:
+            impostor_group = None
+
         auth_user = None
         try:
             # Admin logging as user?
-            admin, uuser = [ uname.strip() for uname in username.split(" as ") ]
+            admin, uuser = [uname.strip() for uname in username.split(" as ")]
 
             # Check if admin exists and authenticates
-            admin_obj = User.objects.get(username=admin)
-            if (admin_obj.is_superuser or (IMPOSTOR_GROUP and IMPOSTOR_GROUP in admin_obj.groups.all())) and admin_obj.check_password(password):
-                try:
-                    auth_user = User.objects.get(username=uuser)
-                except User.DoesNotExist:
-                    auth_user = User.objects.get(email=uuser)
+            try:
+                if admin and hasattr(User, 'email'):
+                    admin_obj = User.objects.get(email=admin)
+            except User.DoesNotExist:
+                if hasattr(User, 'username'):
+                    admin_obj = User.objects.get(username=admin)
 
-            if auth_user:
-                # Superusers can only be impersonated by other superusers
-                if auth_user.is_superuser and not admin_obj.is_superuser:
-                    auth_user = None
-                    raise Exception("Superuser can only be impersonated by a superuser.")
+            if admin_obj:
+                if (admin_obj.is_superuser or (impostor_group and impostor_group in admin_obj.groups.all())) \
+                        and admin_obj.check_password(password):
+                    try:
+                        if uuser and hasattr(User, 'email'):
+                            auth_user = User.objects.get(email=uuser)
+                    except User.DoesNotExist:
+                        if hasattr(User, 'username'):
+                            auth_user = User.objects.get(username=uuser)
 
-                # Try to find request object and maybe be lucky enough to find IP address there
-                request = find_request()
-                ip_addr = ''
-                if request:
-                    ip_addr = request.META.get('HTTP_X_FORWARDED_FOR', request.META.get('HTTP_X_REAL_IP', request.META.get('REMOTE_ADDR', '')))
-                    # if there are several ip addresses separated by comma
-                    # like HTTP_X_FORWARDED_FOR returns,
-                    # take only the first one, which is the client's address
-                    if ',' in ip_addr:
-                        ip_addr = ip_addr.split(',', 1)[0].strip()
-                log_entry = ImpostorLog.objects.create(impostor=admin_obj, imposted_as=auth_user, impostor_ip=ip_addr)
+                if auth_user:
+                    # Superusers can only be impersonated by other superusers
+                    if auth_user.is_superuser and not admin_obj.is_superuser:
+                        auth_user = None
+                        raise Exception("Superuser can only be impersonated by a superuser.")
 
-                if log_entry.token and request:
-                    request.session['impostor_token'] = log_entry.token
+                    # Try to find request object and maybe be lucky enough to find IP address there
+                    request = find_request()
+                    ip_addr = ''
+                    if request:
+                        ip_addr = request.META.get('HTTP_X_FORWARDED_FOR',
+                                                   request.META.get('HTTP_X_REAL_IP',
+                                                                    request.META.get('REMOTE_ADDR', '')))
+                        # if there are several ip addresses separated by comma
+                        # like HTTP_X_FORWARDED_FOR returns,
+                        # take only the first one, which is the client's address
+                        if ',' in ip_addr:
+                            ip_addr = ip_addr.split(',', 1)[0].strip()
+                            log_entry = ImpostorLog.objects.create(impostor=admin_obj, imposted_as=auth_user,
+                                                                   impostor_ip=ip_addr)
 
-        except: # Nope. Do nothing and let other backends handle it.
+                    if log_entry.token and request:
+                        request.session['impostor_token'] = log_entry.token
+
+        except:  # Nope. Do nothing and let other backends handle it.
             pass
         return auth_user
 
     def get_user(self, user_id):
+
+        try:
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+        except ImportError:
+            from django.contrib.auth.models import User
+
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
